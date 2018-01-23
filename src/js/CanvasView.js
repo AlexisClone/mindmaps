2184e477239e829d289f88d62b85d832dba9ae2f
LucasLelievre <lucas.lelievre98@gmail>
2018-01-23 15:51:52 +0100

    minor update

--- src/js/CanvasView.js
+++ src/js/CanvasView.js
@@ -529,17 +529,22 @@ mindmaps.DefaultCanvasView = function() {
     // creator.detach();
 
     // delete all DOM below
+
+    //delete the links created from him
     if (node.getSymbolicLinks().length > 0){
       for (var i = 0; i < node.getSymbolicLinks().length; i++){
-        deleteLink(node, node.getSymbolicLinks()[i])
+        console.log(node);
+        deleteLink(node, node.getSymbolicLinks()[i]);
       }
     }
+
+    //delete the links created to him
     var root = node.getRoot();
     root.forEachDescendant(function(parent) {
       if(parent.includeSymbolicLink(node)){
         deleteLink(parent, node);
       }
-    }
+    });
 
     var $node = $getNode(node);
     $node.remove();
